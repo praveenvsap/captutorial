7acc3f5396f53e0c6e212daa83206ca26c5e5f6a
Milton Chandradas <milton.chandradas@live.com>
2023-05-06 06:06:59 -0400

    cds query language...

--- app1/srv/demo-service.js
+++ app1/srv/demo-service.js
@@ -1,19 +1,47 @@
 const cds = require("@sap/cds");
 
 module.exports = (srv) => {
-   srv.before("*", (req) => {
+  const { Employees, Departments } = srv.entities;
 
-      let results = {};
-      results.user = req.user.id;
+  srv.before("*", (req) => {
+    //   let results = {};
+    //   results.user = req.user.id;
+    //   if (req.user.hasOwnProperty("locale")) {
+    //      results.locale = req.user.locale;
+    //   }
+    //   results.scopes = {};
+    //   results.scopes.identified = req.user.is("identified-user");
+    //   results.scopes.authenticated = req.user.is("authenticated-user");
+    //   results.scopes.Admin = req.user.is("Admin");
+    //   results.tenant = req.user.tenant;
+    //   console.log("User Details: ", results);
+  });
 
-      if (req.user.hasOwnProperty("locale")) {
-         results.locale = req.user.locale;
-      }
-      results.scopes = {};
-      results.scopes.identified = req.user.is("identified-user");
-      results.scopes.authenticated = req.user.is("authenticated-user");
-      results.scopes.Admin = req.user.is("Admin");
-      results.tenant = req.user.tenant;
-      console.log("User Details: ", results);
-   });
+  srv.on("READ", Employees, async (req, next) => {
+    await next();
+
+    // return await SELECT.from(Employees);
+  });
+
+  srv.on("READ", Departments, async (req, next) => {
+    await next();
+
+    // return await SELECT.from(Departments);
+
+    // if (!req.query.SELECT.columns)
+    //    await next();
+
+    // const expandIndex = req.query.SELECT.columns.findIndex(
+    //    ({ expand, ref }) => expand && ref[0] === "employees"
+    // );
+
+    // if (expandIndex < 0) await next();
+
+    // return await SELECT.from(Departments, (department) => {
+    //    department.name,
+    //       department.employees((employee) => {
+    //          employee.name;
+    //       });
+    // });
+  });
 };
