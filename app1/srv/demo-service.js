395ac2b12d4c436ac260b2ae6813113388ecb00c
praveenvsap <praveen.varriam@sap.com>
2023-10-31 10:35:44 -0400

    update 5

--- app1/srv/demo-service.js
+++ app1/srv/demo-service.js
@@ -4,17 +4,17 @@ module.exports = (srv) => {
   const { Employees, Departments } = srv.entities;
 
   srv.before("*", (req) => {
-    //   let results = {};
-    //   results.user = req.user.id;
-    //   if (req.user.hasOwnProperty("locale")) {
-    //      results.locale = req.user.locale;
-    //   }
-    //   results.scopes = {};
-    //   results.scopes.identified = req.user.is("identified-user");
-    //   results.scopes.authenticated = req.user.is("authenticated-user");
-    //   results.scopes.Admin = req.user.is("Admin");
-    //   results.tenant = req.user.tenant;
-    //   console.log("User Details: ", results);
+      let results = {};
+      results.user = req.user.id;
+      if (req.user.hasOwnProperty("locale")) {
+         results.locale = req.user.locale;
+      }
+      results.scopes = {};
+      results.scopes.identified = req.user.is("identified-user");
+      results.scopes.authenticated = req.user.is("authenticated-user");
+      results.scopes.Admin = req.user.is("Admin");
+      results.tenant = req.user.tenant;
+      console.log("User Details: ", results);
   });
 
   srv.on("READ", Employees, async (req, next) => {
